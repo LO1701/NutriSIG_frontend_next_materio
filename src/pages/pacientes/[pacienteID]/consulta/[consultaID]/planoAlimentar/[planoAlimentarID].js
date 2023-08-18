fbb8c018dfab9fd66d47a2c329531c136c745e99
Lurian Ortiz <lurian502@gmail.com>
2023-08-18 20:23:51 -0300

    Alteracoes em planoAlimentar

--- src/pages/pacientes/[pacienteID]/consulta/[consultaID]/planoAlimentar/[planoAlimentarID].js
+++ src/pages/pacientes/[pacienteID]/consulta/[consultaID]/planoAlimentar/[planoAlimentarID].js
@@ -84,16 +84,22 @@ const PlanoID = () => {
     const [rowsPerPage, setRowsPerPage] = useState(5);
     const [page, setPage] = useState(0);
     const [medida, setMedida] = useState([])
+    const [planoAlimentar, setPlanoAlimentar] = useState([])
     const [refeicoes, setRefeicoes] = useState([])
 
     useEffect( async (ctx) => {
         const usuarioAutenticado = await authService.getSession(ctx)
 
+        // Busca informações do plano alimentar
+        const endPointPlanoAlimentar = `paciente/consulta/${consultaID}/plano/${planoAlimentarID}`
+        const getPlanoAlimentar = await buscaInformacoes(ctx, endPointPlanoAlimentar)
+        setPlanoAlimentar(getPlanoAlimentar.body)
+
         // Busca todas refeicoes
         const endPointRefeicoes = `${usuarioAutenticado.body.id}/paciente/consulta/plano/${planoAlimentarID}/refeicao`
         const getRefeicoes = await buscaInformacoes(ctx, endPointRefeicoes)
         setRefeicoes(getRefeicoes.body)
-
+console.log(refeicoes)
 
     }, [])
 
@@ -113,11 +119,27 @@ const PlanoID = () => {
         <Grid item xs={12} sm={12}>
           <Card>
             <CardHeader title='Informações do plano alimentar' />
-            <CardContent>
-                <Typography variant='body1'>
-                    info1
-                </Typography>
-            </CardContent>
+              <Grid item xs={12} sm={6}>
+                  <CardContent>
+                      <Typography variant='body1'>
+                          {planoAlimentar?.nome}
+                      </Typography>
+                  </CardContent>
+              </Grid>
+              <Grid item xs={12} sm={6} >
+                <CardContent>
+                    <Typography variant='body1'>
+                        {planoAlimentar?.teto_kcal} kcal
+                    </Typography>
+                </CardContent>
+              </Grid>
+              <Grid item xs={12} sm={6} >
+                <CardContent>
+                    <Typography variant='body1'>
+                        {planoAlimentar?.createdAt}
+                    </Typography>
+                </CardContent>
+              </Grid>
             <CardActions className='card-action-dense' sx={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <Button onClick={() => {window.location.replace(`http://localhost:3000/pacientes/${pacienteID}/consulta/${consultaID}/anamnese`)}}>Editar</Button>
             </CardActions>
@@ -129,7 +151,7 @@ const PlanoID = () => {
         <Container>
           <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5} sx={{marginTop: '25px'}}>
             <Typography variant="h5" gutterBottom>
-              Medidas Antropométricas
+              Refeições
             </Typography>
             <Button variant="contained" onClick={() => console.log('oi')}>
               <ScaleBathroom sx={{marginRight: 1, fontSize: '1.375rem', marginBottom: 1}}/>
@@ -137,66 +159,47 @@ const PlanoID = () => {
             </Button>
           </Stack>
 
-          {medida?.length > 0? (
+          {refeicoes?.length > 0? (
             <>
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-                  <TableHead>
-                      <TableRow>
-                          <TableCell>Data de cadastro</TableCell>
-                          <TableCell>Classificação</TableCell>
-                          <TableCell>Peso</TableCell>
-                          <TableCell>IMC</TableCell>
-                          <TableCell sx={{width: 5}}>Editar</TableCell>
-                      </TableRow>
-                  </TableHead>
-                  <TableBody>
-                      {medida?.map(row => (
-                          <TableRow
-                              key={row.id}
-                              sx={{
-                                  '&:last-of-type td, &:last-of-type th': {
-                                  border: 0
-                                  }
-                              }}
-                          >
-                              <TableCell component='th' scope='row'>
-                                  {row.dataDeCriacao}
-                              </TableCell>
-                              <TableCell component='th' scope='row'>
-                                  {row.classificacao_imc}
-                              </TableCell>
-                              <TableCell component='th' scope='row'>
-                                  {row.peso_atual} kg
-                              </TableCell>
-                              <TableCell component='th' scope='row'>
-                                  {row.imc_atual}
-                              </TableCell>
-                              <TableCell align="center">
-                                  <IconButton size='small' onClick={() => {router.push(`${paciente?.id}/consulta/${row.id}/perfil`)}}>
-                                      <PencilBoxMultiple />
-                                  </IconButton>
-                              </TableCell>
-                          </TableRow>
-                      ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                component="div"
-                count={medida.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-              />
+              {refeicoes?.map(row => (
+                  <Grid container spacing={6}>
+                  <Grid item xs={12} sm={12}>
+                    <Card>
+                      <CardHeader title={row?.nome} />
+                        <Grid item xs={12} sm={12}>
+                            <CardContent>
+                                <Typography variant='body1'>
+                                    Turno: {row?.turno}
+                                </Typography>
+                            </CardContent>
+                        </Grid>
+                        <Grid item xs={12} sm={6} >
+                          <CardContent>
+                              <Typography variant='body1'>
+                                  Horário: {row?.horario}
+                              </Typography>
+                          </CardContent>
+                        </Grid>
+                        <Grid item xs={12} sm={6} >
+                          <CardContent>
+                              <Typography variant='body1'>
+                                  Descrição: {row?.descricao}
+                              </Typography>
+                          </CardContent>
+                        </Grid>
+                      <CardActions className='card-action-dense' sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                          <Button onClick={() => {window.location.replace(`http://localhost:3000/pacientes/${pacienteID}/consulta/${consultaID}/anamnese`)}}>Adicionar alimentos</Button>
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                </Grid>
+              ))}
+            
             </>
             ):(
               <Typography variant="subtitle1" gutterBottom sx={{display: 'flex', justifyContent: 'center', margin:10}}>
                 <CloseBoxMultiple sx={{marginRight: 2, fontSize: '1.375rem',}}/>
-                Nenhuma medida antropométrica cadastrada
+                Nenhuma refeição cadastrada
               </Typography>
             )}
         </Container>
